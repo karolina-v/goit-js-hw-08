fc1723a27b4b230c21300b4f84f5b5b1172f5b33
karolina-v <vazniak96@gmail.com>
2022-02-02 00:27:45 +0200

    hw 8
    + form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+addData();
+
+const formData = {
+    email: formEl.email.value,
+    message: formEl.message.value,    
+    };
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value,
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+   
+    const dataSubmit = {
+        email: e.currentTarget.email.value,
+        message: e.currentTarget.message.value,
+    }
+    console.log(dataSubmit);
+    
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    formEl.reset();
+};
+
+function addData() {
+     const localData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+    
+    if (!localData)
+        return;
+    if (localData.email)
+        formEl.email.value = localData.email;        
+    if (localData.message)
+        formEl.message.value = localData.message;  
+}
